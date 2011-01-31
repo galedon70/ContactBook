e48edac233489085d88c0029307f7f642a1904f6
scott <scott@Tourniquet>
2011-01-31 11:24:41 -0600

    A few comments and next steps added to the README

--- src/com/kcjavadev/contactbook/ContactBook.java
+++ src/com/kcjavadev/contactbook/ContactBook.java
@@ -1,17 +1,36 @@
 package com.kcjavadev.contactbook;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+/**
+ * Typically good to document your classes to explain what they are for.
+ * 
+ * ContactBook is the main entry into the app.  This sets everything up and communicates
+ * with the client to get input.
+ * 
+ * @author scott
+ *
+ */
 public class ContactBook	{
 	
+	// NOTE : what is the purpose of having these?
 	String name, address;
 	
+	private HashMap<String,String> contacts = new HashMap<String,String>();
+	
+	// TODO : you can move to adding Person objects instead of using the HashMap
+	private List<Person> persons;
+
+	// Typically want to make constructors public unless you definitely don't want others accessing.
 	ContactBook(){
 		name = "";
 		address = "";
+		// you can initialize parameters in the constructor or when you define them
+		persons = new ArrayList<Person>();
 	}
 	
-	private HashMap<String,String> contacts = new HashMap<String,String>();
 	
     public void addContact(String a, String n){
     	name = n;
@@ -27,6 +46,10 @@ public class ContactBook	{
         }
     }
 
+    /**
+     * Main (currently only) way into the application.  
+     * @param args
+     */
 	public static void main( String[] args ) {
         System.out.println( "Staring application" );
         
