7452a513ec3d21643fcc8d3d4811004ba357c811
galedon70 <galedon70@hotmail.com>
2011-01-31 13:41:48 -0800

    Updated to use ArrayList instead of HashMap

--- src/com/kcjavadev/contactbook/ContactBook.java
+++ src/com/kcjavadev/contactbook/ContactBook.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Typically good to document your classes to explain what they are for.
  * 
  * ContactBook is the main entry into the app.  This sets everything up and communicates
  * with the client to get input.
@@ -15,34 +14,25 @@ import java.util.List;
  */
 public class ContactBook	{
 	
-	// NOTE : what is the purpose of having these?
-	String name, address;
-	
-	private HashMap<String,String> contacts = new HashMap<String,String>();
-	
-	// TODO : you can move to adding Person objects instead of using the HashMap
-	private List<Person> persons;
+
+	//List of Persons
+               private List<Person> contacts;
 
 	// Typically want to make constructors public unless you definitely don't want others accessing.
-	ContactBook(){
-		name = "";
-		address = "";
-		// you can initialize parameters in the constructor or when you define them
-		persons = new ArrayList<Person>();
+	public ContactBook(){
+
+		contacts = new ArrayList<Person>();
 	}
 	
 	
-    public void addContact(String a, String n){
-    	name = n;
-    	address = a;
-    	contacts.put(name, address);
+    public void addContact(Person person){
+          contacts.add(person);
     }
 
     public void printContacts( ) {
         // this is the new way to do a for-loop.  Java converts the Set to an iterator and does the base work for you.
-        for ( String name : contacts.keySet() ) {
-            // a map is just a key/value pair.  The name is the key so contacts.get( name ) gets the associated city
-            System.out.println( name + " : " + contacts.get( name ) );
+        for ( Person person : contacts ) {
+            System.out.println( person.getName() + " : " + person.getAddress() );
         }
     }
 
@@ -51,17 +41,28 @@ public class ContactBook	{
      * @param args
      */
 	public static void main( String[] args ) {
-        System.out.println( "Staring application" );
+        System.out.println( "Starting application" );
         
         // initialize a new class to work with
-        ContactBook cb = new ContactBook();
+        ContactBook contacBook = new ContactBook();
         
         System.out.println( "Creating initial contact book" );
-        cb.addContact("Test Name1","Test Address1");
-        cb.addContact("Test Name2", "Test Address2");
+
+        Person person = new Person;
+        person.setName("Jo Blow");
+        person.setCity("JoVille");
+        person.SetState("CA");
+
+        contactBook.addContact(person);
+
+        person.setName("Suzie Smiles");
+        person.setCity("Happy Town");
+        person.SetState("TX");
+
+        contactBook.addContact(person);
         
         // now print the entries
-        cb.printContacts( );
+        contactBook.printContacts( );
     	}
     
    
